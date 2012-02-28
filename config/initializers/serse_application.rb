003b1d741cddc952acf1a5ac8b90acca30882533
Ward Vandewege <ward@jhvc.com>
2012-02-27 23:07:35 -0500

    do the usual trick to load rails3_acts_as_paranoid

--- config/initializers/serse_application.rb
+++ config/initializers/serse_application.rb
@@ -1,5 +1,7 @@
  
  # We load cure_acts_as_versioned in the Gemfile, but that gem really defines 'acts_as_versioned'
  require 'acts_as_versioned'
+ # We load cure_rails3_acts_as_paranoid in the Gemfile, but that gem really defines 'rails3_acts_as_paranoid'
+ require 'rails3_acts_as_paranoid'
 
 Time::DATE_FORMATS[:pretty] = lambda { |time| time.strftime("%a, %b %e %Y") }
