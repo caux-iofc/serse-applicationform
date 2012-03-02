1c883b31e210e6b5fb7f0ddffeb959a328f1bc32
Ward Vandewege <ward@jhvc.com>
2012-03-02 11:59:13 -0500

    Make deployment scripts copy the vendor/bundle directory from the previous deployed version of the site, if posible. This leads to a massive speedup during deployment.

--- config/deploy.production.rb
+++ config/deploy.production.rb
@@ -17,22 +17,30 @@ desc "Clean up old releases"
 set :keep_releases, 5
 before("deploy:cleanup") { set :use_sudo, false }
 
-after "deploy:symlink", "deploy:copy_files", :roles => :app
+after "deploy:symlink", "deploy:copy_vendor_bundle_dir", :roles => :app 
+after "deploy:copy_vendor_bundle_dir", "deploy:copy_files", :roles => :app
 after "deploy:update", "deploy:migrate", :roles => :db
 after :deploy, 'deploy:cleanup', :roles => :app
 
 namespace :deploy do
+  desc "Copy vendor/bundle to the new directory"
+  task :copy_vendor_bundle_dir, :on_error => :continue do
+    prev_uploads_dir = "#{previous_release}/vendor/bundle"
+    run "[ -d #{prev_uploads_dir} ] && cp -pr #{prev_uploads_dir} #{current_path}/vendor/"
+  end 
   desc "Put a few files in place, ensure correct file ownership"
   task :copy_files, :roles => :app,  :except => { :no_release => true } do
     # Copy a few files/make a few symlinks
     run "cp /home/passenger/database.yml-production #{current_path}/config/database.yml"
     run "cp /home/passenger/secret_token.rb-production #{current_path}/config/initializers/secret_token.rb"
+    run "cp /home/passenger/hoptoad.rb-production #{current_path}/config/initializers/hoptoad.rb"
     run "cp /home/passenger/production.rb-production #{current_path}/config/environments/production.rb"
     # Ensure correct ownership of a few files
     run "chown www-data:www-data #{current_path}/config/environment.rb"
     run "chown www-data:www-data #{current_path}/config.ru"
-    # Do the bundle install thing -- this is ... slow
-    run "mkdir #{current_path}/vendor/bundle"
+    # Do the bundle install thing -- this is ridiculously slow if the :copy_vendor_bundle_dir task
+    # was unable to copy the bundle directory
+    run "[ ! -d #{current_path}/vendor/bundle ] && mkdir #{current_path}/vendor/bundle || echo 'vendor/bundle exists, good'"
     run "chown www-data:www-data #{current_path}/vendor/bundle"
     run "chown www-data:www-data #{current_path}"
     run "cd #{current_path}; sudo -u www-data bundle --deployment install"
