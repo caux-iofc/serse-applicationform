599ccd88ca9b07662f157886713fe3fc16af1c91
Ward Vandewege <ward@jhvc.com>
2012-03-18 14:41:05 -0400

    add length limits to address fields

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,11 +3,16 @@ class Address < ActiveRecord::Base
   belongs_to :country
   belongs_to :online_application
 
-  validates :street1, :presence => true
-  validates :city, :presence => true
+  validates :street1, :presence => true, :length => { :maximum => 100 }
+  validates :street2, :length => { :maximum => 100 }
+  validates :street3, :length => { :maximum => 100 }
+  validates :city, :presence => true, :length => { :maximum => 30 }
+  validates :state, :length => { :maximum => 30 }
+  validates :postal_code, :length => { :maximum => 20 }
   validates :postal_code, :format => { :with => /^(.{2,}|)$/, :message => I18n.t(:postal_code_invalid) }
   validates :country_id, :presence => true
   validates :other_country, :presence => true, :if => "country_id == 0"
+  validates :other_country, :length => { :maximum => 100 }, :if => "country_id == 0"
 
 end
 
