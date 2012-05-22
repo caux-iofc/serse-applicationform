a7b00fb779315dd0cd9d405a44942ecf3dd4d86a
Ward Vandewege <ward@jhvc.com>
2012-05-22 07:49:07 -0400

    Apparently IE7 does not return a referer field (?!).
    That makes our system mailer break where it refers to said referer
    field. Add to_s to all request references to guard against this.
    
    This closes bug #220

--- app/mailers/system_mailer.rb
+++ app/mailers/system_mailer.rb
@@ -6,18 +6,18 @@ class SystemMailer < ActionMailer::Base
     @message = message
 
     @connection = ''
-    @connection += "remote ip     = " + the_request.env['REMOTE_ADDR'] + "\n"
-    @connection += "URI           = " + the_request.env['PATH_INFO'] + "\n"
+    @connection += "remote ip     = " + the_request.env['REMOTE_ADDR'].to_s + "\n"
+    @connection += "URI           = " + the_request.env['PATH_INFO'].to_s + "\n"
     @connection += "host          = " + ROOT_URL + "\n"
     @connection += "date          = " + Time.now().to_s + "\n"
 
     @server = ''
-    @server += "REQUEST_URI       = " + the_request.env['REQUEST_URI'] + "\n"
-    @server += "QUERY_STRING      = " + the_request.env['QUERY_STRING'] + "\n"
-    @server += "REMOTE_ADDR       = " + the_request.env['REMOTE_ADDR'] + "\n"
-    @server += "HTTP_USER_AGENT   = " + the_request.env['HTTP_USER_AGENT'] + "\n"
-    @server += "HTTP_REFERER      = " + the_request.env['HTTP_REFERER'] + "\n"
-    @server += "REQUEST_METHOD    = " + the_request.env['REQUEST_METHOD'] + "\n"
+    @server += "REQUEST_URI       = " + the_request.env['REQUEST_URI'].to_s + "\n"
+    @server += "QUERY_STRING      = " + the_request.env['QUERY_STRING'].to_s + "\n"
+    @server += "REMOTE_ADDR       = " + the_request.env['REMOTE_ADDR'].to_s + "\n"
+    @server += "HTTP_USER_AGENT   = " + the_request.env['HTTP_USER_AGENT'].to_s + "\n"
+    @server += "HTTP_REFERER      = " + the_request.env['HTTP_REFERER'].to_s + "\n"
+    @server += "REQUEST_METHOD    = " + the_request.env['REQUEST_METHOD'].to_s + "\n"
 
     mail(:subject => "[#{ROOT_URL}] validation failure")
   end
