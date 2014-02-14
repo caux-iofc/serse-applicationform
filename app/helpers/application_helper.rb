f500e28b292fab04bd1daafadf7909edb38c4988
Ward Vandewege <ward@jhvc.com>
2014-02-13 22:34:50 -0500

    A few fixes for rails 3.2

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,16 +18,16 @@ module ApplicationHelper
 
       header_message = "Error!"
       header_message = options[:header_message] if options.has_key?(:header_message)
-      error_messages = raw(objects.map { |object| 
-        object.errors.each_with_index.map {|msg| 
+      error_messages = raw(objects.map { |object|
+        object.errors.each_with_index.map {|msg|
           # If the translation field is deliberately empty, do not print it
-          if (I18n.t(msg[0][0]) != '') then
-            content_tag(:li, raw("<strong>") + I18n.t(msg[0][0].to_s.gsub(/\./,'_')) + raw('</strong> ') + msg[0][1]) 
+          if (I18n.t(msg[0]) != '') then
+            content_tag(:li, raw("<strong>") + I18n.t(msg[0].to_s.gsub(/\./,'_')) + raw('</strong> ') + msg[1])
           else
-            content_tag(:li, msg[0][1]) 
+            content_tag(:li, msg[1])
           end
         } 
-      })
+      }.join(''))
 
       content_tag(:table,
         content_tag(:tr,
@@ -50,7 +50,7 @@ module ApplicationHelper
   # This is a workaround. 
   # Ward, 2012-02-08
   # Note - for Ruby 1.9, you'll need to modify to class: "field_with_errors"
-  # Cf. http://lukecarrier.me/2011/08/rails-3-0-select-tags-and-field_with_errors/
+  # Cf. http://luke.carrier.im/2011/08/rails-3-0-select-tags-and-field_with_errors/
   def field_with_errors(object, method, &block)
     if block_given? then
       if object.errors[method].empty? then
